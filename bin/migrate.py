89187c09cb396b03ebbc1e2b52da6561ed9e690b
Doug Friedel <friedel@illinois.edu>
2022-01-25 15:10:50 -0600

    initial version of the migration code

---
+++ bin/migrate.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import filemgmt.compare_utils as compare
+from filemgmt import migrate
+
+def parse_cmd_line(argv):
+    """ Parse command line arguments
+
+        Parameters
+        ----------
+        args : command line arguments
+
+        Returns
+        -------
+        Dictionary continaing the command line arguments
+    """
+    epilog = """\
+The files to be migrated can be specified in multiple ways:
+    - relpath will select all files at or below the given path
+    - reqnum will select all files of a given reqnum
+      - unitname (must also have a reqnum supplied) will select all files from the given
+        reqnum/unitname
+      - attnum (must have reqnum, but not necessarily unitname supplied) will select all files
+        from the given reqnum/(unitname)/attnum
+    - pfwid will select all files from the given pfw_attempot_id(s), note that multiple
+      pfw_attempt_ids can be given as a comma separated list
+    - tag will select all files from all pfw_attempt_ids linked to the given tag
+
+All of the above are mutually exclusive selection criteria.
+The use of the date_range argument can be used to narrow the selection criteria.
+The following are all valid ways to select the files:
+    --reqnum 1115
+    --reqnum 1115 --attnum 5
+    --pfwid 123456
+    --pfwid 123456,789012,345678
+    --tag Y1A2_JUNK
+"""
+
+    parser = argparse.ArgumentParser(description='Migrate files from one filesystem to another, performing integrity checks of all files',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=epilog)
+    parser.add_argument('--des_services', action='store', help='Services file.')
+    parser.add_argument('--section', '-s', action='store', help='Must be specified if DES_DB_SECTION is not set in environment')
+    parser.add_argument('--archive', action='store', default='desar2home', help='archive_name from file_archive_info table for the files to be moved')
+    parser.add_argument('--destination', action='store', required=True, help='Destination folder for the files.')
+    parser.add_argument('--current', action='store', default=None, help='Section of the current path that will be replaces by the `destination` path. For example --current=\'OPS/\' --destination=\'taiga/\' will move the selected files from <archive root>/OPS to <archive root>/taiga. If left empy the `destinataion` will be prepended to the current directory --destination=\'taiga/\' will move the selected files from <archive root>/ to <archive root>/taiga.')
+    parser.add_argument('--reqnum', action='store', help='Request number to search for')
+    parser.add_argument('--relpath', action='store', help='relative path on disk within archive (no archive root)')
+    parser.add_argument('--unitname', action='store', help='Unit name to search for')
+    parser.add_argument('--attnum', action='store', help='Attempt number to search for')
+    parser.add_argument('--verbose', action='store_true', help='print differences between db and disk')
+    parser.add_argument('--debug', action='store_true', help='print all files, recommend >= 300 char wide terminal')
+    parser.add_argument('--script', action='store_true', help='Print only if there are errors, usefule for running in loops in scripts')
+    parser.add_argument('--pfwid', action='store', help='pfw attempt id to search for')
+    parser.add_argument('--silent', action='store_true', help='Run with minimal printing, only print ERROR or OK')
+    #parser.add_argument('--date_range', action='store', help='Date range of submit time in YYYY-MM-DD format. If date is given then only data from that date are checked, if two comma separated dates are given then all data from between those dates (inclusive) are checked.')
+    parser.add_argument('--pipeline', action='store', help='Compare data from a specific pipeline (subpipeprod in pfw_attempt), only used in conjunction with date_range')
+    parser.add_argument('--tag', action='store', help='Compare all data from a specific tag (this can take a long time)')
+    parser.add_argument('--start_at', action='store', help='Index to start at (1 based), useful for doing checking in chunks.', type=int, default=1)
+    parser.add_argument('--end_at', action='store', help='Index to end at (1 based), useful for doing checking in chunks.', type=int, default=0)
+    parser.add_argument('--dbh', action='store', help=argparse.SUPPRESS) # used internally
+    parser.add_argument('--log', action='store', help='Log file to write to, default is to write to sdtout')
+    cargs = parser.parse_args(argv)
+    if cargs.script:
+        cargs.verbose = False
+    return cargs
+
+def main():
+    """ Main program module
+
+    """
+    args = parse_cmd_line(sys.argv[1:])
+    if args.log is not None:
+        stdp = compare.Print(args.log)
+        sys.stdout = stdp
+    ret = migrate.run_migration(args)
+    if args.log is not None:
+        sys.stdout.flush()
+        sys.stdout = stdp.close()
+    sys.exit(ret)
+
+if __name__ == "__main__":
+    main()
---
+++ python/filemgmt/migrate.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+""" Module to migrate files from one file system to another.
+
+"""
+import os
+import shutil
+from pathlib import Path
+
+from despymisc import miscutils
+import filemgmt.disk_utils_local as diskutils
+import filemgmt.db_utils_local as dbutils
+import filemgmt.compare_utils as compare
+
+def migrate(files_from_db, current, destination, archive_root):
+    results = []
+    for fname, items in files_from_db.items():
+        if current is not None:
+            dst = items['path'].replace(current, destination)
+        else:
+            dst = destination + items['path']
+        shutil.copy2(os.path.join(archive_root, items['path'], fname), os.path.join(archive_root, dst, fname))
+        (_, filename, compress) = miscutils.parse_fullname(fname, miscutils.CU_PARSE_PATH | miscutils.CU_PARSE_FILENAME | miscutils.CU_PARSE_COMPRESSION)
+        results[fname] = {'pth': dst, 'fn':filename, 'comp':compress, 'orig': items['path']}
+    return results
+
+def do_migration(dbh, args):
+    """ Method to migrate the data
+
+        Parameters
+        ----------
+        args : list of command line arguments
+
+        Returns
+        -------
+        the result
+    """
+    archive_root, _, relpath, _, pfwid = compare.gather_data(dbh, args)
+    if not relpath:
+        print(f'  Connot do migration for pfw_attempt_id, no relpath found {pfwid}')
+        return 1
+    newpath = None
+    if args.current is not None:
+        newpath = relpath.replace(args.current, args.destination)
+    else:
+        newpath = os.path.join(args.destination, relpath)
+    if newpath == relpath:
+        print(f"  ERROR: new path is the same as the original {newpath} == {relpath}")
+        return 1
+    newarchpath = os.path.join(archive_root, newpath)
+    #print archive_root
+    if args.debug:
+        print("From DB")
+    files_from_db, db_duplicates = dbutils.get_files_from_db(dbh, relpath, args.archive, pfwid, None, debug=args.debug)
+    # make the root directory
+    path = Path(newarchpath)
+    path.mkdir(parents=True, exist_ok=True)
+
+    newloc = migrate(files_from_db, args.current, args.destination, archive_root)
+    upsql = "update file_archive_info set path=:pth where filename=:fn and compression=:comp"
+    curs = dbh.cursor()
+    curs.executemany(upsql, newloc)
+    dbh.commit()
+    # get new file info from db
+    files_from_db, db_duplicates = dbutils.get_files_from_db(dbh, newpath, args.archive, pfwid, None, debug=args.debug)
+    files_from_disk, duplicates = diskutils.get_files_from_disk(newpath, archive_root, True, args.debug)
+
+    comparison_info = diskutils.compare_db_disk(files_from_db, files_from_disk, duplicates, True, args.debug, archive_root)
+    error = False
+    if len(comparison_info['dbonly']) > 0:
+        error = True
+        print(f"Error {len(comparison_info['dbonly']):d} files found only in the DB")
+    if len(comparison_info['diskonly']) > 0:
+        error = True
+        print(f"Error {len(comparison_info['diskonly']):d} files only found on disk")
+    if len(comparison_info['path']) > 0:
+        error = True
+        print(f"Error {len(comparison_info['path']):d} files have mismatched paths")
+    if len(comparison_info['filesize']) > 0:
+        error = True
+        print(f"Error {len(comparison_info['filesize']):d} files have mismatched file sizes")
+    if len(comparison_info['md5sum']) > 0:
+        error = True
+        print(f"Error {len(comparison_info['md5sum']):d} files have mismatched md5sums")
+    if error:
+        print("Error summary")
+        compare.diff_files(comparison_info, files_from_db, files_from_disk, True, True, duplicates, db_duplicates)
+        upsql = "update file_archive_info set path=:orig where filename=:fn and compression=:comp"
+        curs = dbh.cursor()
+        curs.executemany(upsql, newloc)
+        return 1
+    # remove old files
+    rml = []
+    for item in newloc:
+        fname = item['fn']
+        if item['comp'] is not None:
+            fname += item['comp']
+        rml.append(os.path.join(archive_root, item['orig'], fname))
+    for r in rml:
+        print(r)
+    ok = False
+    while not ok:
+        res = input("Delete the above files[y/n]?")
+        if res.lower() == 'y':
+            for r in rml:
+                os.remove(r)
+            return 0
+        if res.lower() == 'n':
+            return 0
+
+def multi_migrate(dbh, pfwids, args):
+    """ Method to iterate over pfw_attempt_id's and run the migration script
+
+        Parameters
+        ----------
+        dbh : database handle
+        pfwids : result of querying a table for pfw_attempt_ids, usually a list of single element tuples
+        args : an instance of Args containing the command line arguments
+
+        Returns
+        -------
+        A summary of the results of do_migration
+    """
+    count = 0
+    length = len(pfwids)
+    if args.start_at > length or args.start_at < 1:
+        print("Error: Starting index is beyond bounds of list.")
+        return 1
+    offset = int(args.start_at) - 1
+    if args.end_at != 0:
+        if args.end_at < args.start_at:
+            print("Error: Ending index is less than starting index.")
+            return 1
+        if args.end_at > length:
+            print("Error: Ending index is beyond bounds of list.")
+            return 1
+        pfwids = pfwids[offset:args.end_at]
+    else:
+        pfwids = pfwids[offset:]
+    for i, pdwi in enumerate(pfwids):
+        print(f"--------------------- Starting {i + 1 + offset:d}/{length:d} ---------------------")
+        args.pfwid = pdwi
+        count += do_migration(dbh, args)
+    return count
+
+
+def run_migration(args):
+    """ Method to migrate the files
+    """
+    (args, pfwids) = compare.determine_ids(args)
+    # if dealing with a date range then get the relevant pfw_attempt_ids
+    if args.date_range:
+        if not pfwids:
+            return 0
+        return multi_migrate(args.dbh, pfwids, args)
+
+    # if only a single comparison was requested (single pfw_attempt_id, triplet (reqnum, uniname, attnum), or path)
+    if not pfwids:
+        return do_migration(args.dbh, args)
+    if len(pfwids) == 1:
+        args.pfwid = pfwids[0]
+        return do_migration(args.dbh, args)
+    pfwids.sort() # put them in order
+    return multi_migrate(args.dbh, pfwids, args)
