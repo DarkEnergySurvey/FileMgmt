221beb0a2ae8e2f6a9bf0a55c15ac2e36110d709
Doug Friedel <friedel@illinois.edu>
2022-02-15 15:35:33 -0600

    initial version of log compacting code

---
+++ bin/compact_logs.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import signal
+import math
+import copy
+import curses
+import queue
+import time
+import multiprocessing as mp
+import datetime
+
+import filemgmt.compare_utils as compare
+from filemgmt import compact_utils as cu
+
+COMPLETE = "Complete"
+
+def parse_cmd_line(argv):
+    """ Parse command line arguments
+
+        Parameters
+        ----------
+        args : command line arguments
+
+        Returns
+        -------
+        Dictionary continaing the command line arguments
+    """
+    epilog = """\
+The files to be migrated can be specified in multiple ways:
+    - pfwid will select all files from the given pfw_attempot_id(s), note that multiple
+      pfw_attempt_ids can be given as a comma separated list
+    - tag will select all files from all pfw_attempt_ids linked to the given tag
+
+All of the above are mutually exclusive selection criteria.
+The use of the date_range argument can be used to narrow the selection criteria.
+The following are all valid ways to select the files:
+    --pfwid 123456
+    --pfwid 123456,789012,345678
+    --tag Y1A2_JUNK
+"""
+
+    parser = argparse.ArgumentParser(description='Migrate files from one filesystem to another, performing integrity checks of all files',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=epilog)
+    parser.add_argument('--des_services', action='store', help='Services file.')
+    parser.add_argument('--section', '-s', action='store', help='Must be specified if DES_DB_SECTION is not set in environment')
+    parser.add_argument('--archive', action='store', default='desar2home', help='archive_name from file_archive_info table for the files to be moved')
+    parser.add_argument('--reqnum', action='store', help='Not used, but maintined for code compatability.')
+    parser.add_argument('--relpath', action='store', help='Not used, but maintined for code compatability.')
+    parser.add_argument('--unitname', action='store', help='Not used, but maintined for code compatability.')
+    parser.add_argument('--attnum', action='store', help='Not used, but maintined for code compatability.')
+    parser.add_argument('--verbose', action='store_true', help='print differences between db and disk')
+    parser.add_argument('--debug', action='store_true', help='print all files, recommend >= 300 char wide terminal')
+    #parser.add_argument('--script', action='store_true', help='Print only if there are errors, usefule for running in loops in scripts')
+    parser.add_argument('--pfwid', action='store', help='pfw attempt id to search for')
+    parser.add_argument('--silent', action='store_true', help='Run with minimal printing, only print ERROR or OK')
+    parser.add_argument('--date_range', action='store', help='Not used, but maintined for code compatability.')
+    parser.add_argument('--tag', action='store', help='Compare all data from a specific tag (this can take a long time)')
+    parser.add_argument('--start_at', action='store', help='Not used, but maintined for code compatability.', type=int, default=1)
+    parser.add_argument('--end_at', action='store', help='Not used, but maintined for code compatability.', type=int, default=0)
+    parser.add_argument('--dbh', action='store', help=argparse.SUPPRESS) # used internally
+    parser.add_argument('--log', action='store', help='Log file to write to, default is to write to sdtout')
+    parser.add_argument('--parallel', action='store', help='Specify the parallelization of the work, e.g. 3 would spread the work across 3 subprocesses.', type=int, default=1)
+    cargs = parser.parse_args(argv)
+    if cargs.script:
+        cargs.verbose = False
+    return cargs
+
+def printProgressBar(win, iteration, count, length = 100, fill = '█', printEnd = "\n"):
+    """ Print a progress bar
+    """
+    percent = (f"{iteration:d}/{count:d}")
+    filledLength = int(length * iteration // count)
+    pbar = fill * filledLength + '-' * (length - filledLength)
+    win.addstr(2, 0, f"Progress: |{pbar}| {percent}{printEnd}")
+
+def run(inputs):
+    """ Method to launch a multiprocessing run
+    """
+    try:
+        (wn, args, pfwids, event, que) = inputs
+        cu.CompactLogs(wn, args, pfwids, event, que)
+    finally:
+        que.put_nowait(cu.Message(wn, COMPLETE, 0))
+
+def results_error(err):
+    """ Error handling routine
+    """
+    print("Exception raised:")
+    print(err)
+    raise err
+
+def main():
+    """ Main program module
+
+    """
+    start = datetime.datetime.now()
+    args = parse_cmd_line(sys.argv[1:])
+    if args.date_range:
+        print("Date ranges cannot be used with the migration script")
+        return
+    if args.start_at != 1 or args.end_at != 0:
+        print("start_at and end_at cannot be used with the migration script")
+        return
+    if args.relpath:
+        print("relpath cannot be used with migration script")
+        return
+    if args.reqnum or args.attnum or args.unitname:
+        print("Cannot specify reqnum, attnum, or unitname")
+        return
+    if args.log is not None:
+        stdp = compare.Print(args.log)
+        sys.stdout = stdp
+    (args, pfwids) = compare.determine_ids(args)
+    manager = mp.Manager()
+    event = manager.Event()
+
+    def interrupt(x, y):
+        event.set()
+
+    signal.signal(signal.SIGINT, interrupt)
+
+    if args.parallel <= 1:
+        _ = cu.CompactLogs(0, args, pfwids, event)
+    else:
+        if args.parallel > 8:
+            args.parallel = 8
+        args.dbh.close()
+        args.dbh = None
+        npids = len(pfwids)
+        jobs = []
+        for _ in range(args.parallel):
+            jobs.append([])
+        pos = 0
+        while pfwids:
+            jobs[pos].append(pfwids.pop())
+            pos += 1
+            if pos >= args.parallel:
+                pos = 0
+        manager = mp.Manager()
+        queu = manager.Queue()
+        done = [False] * len(jobs)
+        wins = []
+        errors = {}
+        try:
+            stdscr = curses.initscr()
+            curses.cbreak()
+            num_rows, num_cols = stdscr.getmaxyx()
+            step = math.floor(num_rows/len(jobs))
+            for i in range(len(jobs)):
+                wins.append(curses.newwin(step, num_cols, i*step, 0))
+
+            with mp.Pool(processes=len(jobs), maxtasksperchild=1) as pool:
+                _ = [pool.apply_async(run, args=((i, copy.deepcopy(args), jobs[i], event, queu,),), error_callback=results_error) for i in range(len(jobs))]
+                pool.close()
+                #pool.join()
+                while not all(done):
+                    while True:
+                        try:
+                            ms = queu.get_nowait()
+                            if ms.err:
+                                if ms.pfwid not in errors:
+                                    errors[ms.pfwid] = []
+                                errors[ms.pfwid].append(ms.msg)
+                                continue
+                            if ms.msg == COMPLETE:
+                                done[ms.win] = True
+                                wins[ms.win].clear()
+                                wins[ms.win].addstr("Complete\n")
+                            elif ms.msg is not None:
+                                wins[ms.win].clear()
+                                wins[ms.win].addstr(ms.msg + '\n')
+                            else:
+                                printProgressBar(wins[ms.win], ms.iteration, ms.count)
+                            wins[ms.win].refresh()
+                        except queue.Empty:
+                            break
+                    time.sleep(0.2)
+        finally:
+            curses.endwin()
+        if errors:
+            print(f"Issues were encountered in {len(errors)}/{npids} jobs.")
+            for pid, msgs in errors.items():
+                print(f"pfwid: {pid}")
+                for m in msgs:
+                    m = m.strip()
+                    print(f"   {m}")
+        else:
+            print("All tasks accomplished")
+
+    end = datetime.datetime.now()
+    duration = end-start
+    print(f"\nJob took {duration.total_seconds():.1f} seconds")
+
+    if args.log is not None:
+        sys.stdout.flush()
+        sys.stdout = stdp.close()
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
---
+++ python/filemgmt/compact_utils.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+""" Module to migrate files from one file system to another.
+
+"""
+import os
+import tarfile
+
+import filemgmt.filemgmt_db as fmdb
+import filemgmt.db_utils_local as dbutils
+import filemgmt.compare_utils as compare
+
+def removeEmptyFolders(path, removeRoot=True):
+    """ Function to remove empty folders
+    """
+    if not os.path.isdir(path):
+        return
+
+    # remove empty subfolders
+    files = os.listdir(path)
+    if len(files):
+        for f in files:
+            fullpath = os.path.join(path, f)
+            if os.path.isdir(fullpath):
+                removeEmptyFolders(fullpath)
+
+    # if folder empty, delete it
+    files = os.listdir(path)
+    if len(files) == 0 and removeRoot:
+        os.rmdir(path)
+
+class Message:
+    """ Class for passing messages to main process
+    """
+    def __init__(self, window, msg, pfwid, iteration=None, count=None, err=False):
+        self.win = window
+        self.msg = msg
+        self.iteration = iteration
+        self.count = count
+        self.err = err
+        self.pfwid = pfwid
+
+class CompactLogs:
+    """ Class for migrating data
+
+    """
+    def __init__(self, win, args, pfwids, event, que=None):
+        self.pfwids = pfwids
+        if args.dbh is None:
+            args.dbh = fmdb.FileMgmtDB(args.des_services, args.section)
+        args.dbh.dynam_load_ftmgmt()
+
+        self.args = args
+        self.win = win
+        self.event = event
+        self.que = que
+        self.dbh = self.args.dbh
+        self.des_services = self.args.des_services
+        self.section = self.args.section
+        self.archive = self.args.archive
+        self.verbose = self.args.verbose
+        self.debug = self.args.debug
+        self.pfwid = self.args.pfwid
+        self.silent = self.args.silent
+        self.tag = self.args.tag
+        self.archive_root = None
+        self.results = {"null": [],
+                        "comp": []}
+        self.paths = {"null": [],
+                      "comp": []}
+        self.count = 0
+        self.status = 0
+        self.iteration = 0
+        self.halt = False
+        self.number = 0
+        self.length = 1
+        self.tarfile = None
+
+        if not self.pfwids:
+            _ = self.doCompact()
+        elif len(self.pfwids) == 1:
+            self.pfwid = self.pfwids[0]
+            self.args.pfwid = self.pfwid
+            _ = self.doCompact()
+        else:
+            self.pfwids.sort() # put them in order
+            self.multi_compact()
+
+
+    def __del__(self):
+        if self.dbh:
+            self.dbh.close()
+
+    def update(self, msg=None, err=False):
+        """ Method to report the progress of the job
+
+        """
+        if self.silent:
+            return
+        if self.que is not None:
+            if msg is not None:
+                self.que.put_nowait(Message(self.win, f"Processing {self.pfwid}  ({self.number+1}/{self.length})\n{msg}", pfwid=self.pfwid, err=err))
+            else:
+                self.que.put_nowait(Message(self.win, None, self.pfwid, self.iteration, self.count))
+        elif msg is not None:
+            print(msg)
+
+    def check_permissions(self, files_from_db):
+        """ Check the permissions of the initial files to make sure they can be read and written
+        """
+        bad_files = []
+        self.update("Checking file permissions")
+        self.iteration = 0
+        self.update()
+        for fname, items in files_from_db.items():
+            if self.check_status():
+                return False
+            if not os.access(os.path.join(self.archive_root, items['path'], fname), os.R_OK|os.W_OK):
+                bad_files.append(fname)
+            self.iteration += 1
+            self.update()
+
+        if bad_files:
+            with open(f"{self.pfwid}.badperm", 'w', encoding="utf-8") as fh:
+                for f in bad_files:
+                    fh.write(f"{f}\n")
+
+            self.update(f"Some files do not have rw permissions. See {self.pfwid}.badperm for a list.", True)
+            return False
+        return True
+
+    def check_status(self):
+        """ Method to check whether the processing should continue
+        """
+        if self.halt:
+            return True
+        if self.event is not None:
+            if self.event.is_set():
+                self.rollback()
+        return self.halt
+
+    def rollback(self):
+        """ Method to undo any changes if something goes wrong
+        """
+        if self.halt:
+            return
+        self.halt = True
+        if self.status != 0:
+            self.update("The process cannot be interrupted at this stage")
+            return
+
+        self.update("Rolling back any changes...")
+        if self.dbh:
+            self.dbh.rollback()
+        try:
+            os.remove(self.tarfile)
+        except:
+            self.update(f"Could not remove {self.archive_root}/log/{self.tarfile}", True)
+
+    def doCompact(self):
+        """ Method to migrate the data
+
+            Parameters
+            ----------
+            args : list of command line arguments
+
+            Returns
+            -------
+            the result
+        """
+        owd = os.getcwd()
+        try:
+            self.update("Gathering file info from DB")
+            self.archive_root, _, relpath, _, pfwid = compare.gather_data(self.dbh, self.args)
+            curs = self.dbh.cursor()
+            curs.execute(f"select reqnum, unitname, attnum from pfw_attempt where id={pfwid}")
+            (reqnum, uname, attid) = curs.fetchone()
+            if not relpath:
+                self.update(f'  Connot compact logs for pfw_attempt_id, no relpath found {pfwid}', True)
+                return 1
+
+            logroot = os.path.join(self.archive_root, relpath, 'log')
+            files_from_db, _ = dbutils.get_files_from_db(self.dbh, relpath, self.archive, pfwid, 'log', debug=self.debug)
+            self.count = len(files_from_db)
+            os.chdir(logroot)
+            if not self.check_permissions(files_from_db):
+                return 0
+
+            self.update("Tar'ing files")
+            self.iteration = 0
+            self.update()
+            fnames = []
+            self.tarfile = f"log.{uname}_r{reqnum}p{attid:02d}.tar.gz"
+            with tarfile.open(self.tarfile, 'w:gz') as zfh:
+                for fname, items in files_from_db.items():
+                    fnames.append(os.path.join(self.archive_root, items['path'], fname))
+                    name = os.path.join(items['path'].replace(os.getcwd() + '/', ''), fname)
+                    zfh.add(name)
+                    #fnames.append(fname)
+                    self.iteration += 1
+                    self.results['null'].append({'fid', items['id']})
+                    self.update()
+
+
+            self.update("Updating database...")
+            try:
+                if self.results['null'] :
+                    upsql = "delete from file_archive_info where desfile_id=:fid"
+                    curs = self.dbh.cursor()
+                    curs.executemany(upsql, self.results['null'])
+                    _ = self.dbh.register_file_data('logtar', [self.tarfile], self.pfwid, 0, False)
+                    sql = f"select id from desfile where filename='{self.tarfile.replace('.gz', '')} and compression='.gz'"
+                    curs.execute(sql)
+                    fid = curs.fetchone()[0]
+                    sql = f"insert into file_archive_info (filename, compression, archive_name, path, desfile_id) values ('{self.tarfile.replace('.gz', '')}', '.gz', '{self.archive}', '{os.path.join(relpath, 'log')}', {fid}"
+                    curs.execute(sql)
+
+            except:
+                self.update("Error updating the database entries, rolling back any DB changes.", True)
+                self.rollback()
+                raise
+
+            # remove old files
+            cannot_del = []
+            self.status = 1
+            return 0
+            """ self.dbh.commit()
+            self.update("Removing original files")
+            self.iteration = 0
+            self.update()
+            for i, r in enumerate(fnames):
+                try:
+                    os.remove(r)
+                    self.iteration = i + 1
+                    self.update()
+                except:
+                    cannot_del.append(r)
+                    removeEmptyFolders(os.path.join(self.archive_root,relpath,'log'))
+            self.status = 0
+
+            if cannot_del:
+                with open(f"{self.pfwid}.undel", 'w', encoding="utf-8") as fh:
+                    for f in cannot_del:
+                        fh.write(f"    {f}\n")
+                self.update(f"Cannot delete some files. See {self.pfwid}.undel for a list.", True)
+            """
+        finally:
+            os.chdir(owd)
+        return 0
+
+    def multi_compact(self):
+        """ Method to iterate over pfw_attempt_id's and run the migration script
+
+            Parameters
+            ----------
+            dbh : database handle
+            pfwids : result of querying a table for pfw_attempt_ids, usually a list of single element tuples
+            args : an instance of Args containing the command line arguments
+
+            Returns
+            -------
+            A summary of the results of do_migration
+        """
+        self.length = len(self.pfwids)
+
+        for i, pdwi in enumerate(self.pfwids):
+            self.number = i
+            if self.check_status():
+                return
+            self.copied_files = []
+            self.results = {"null": [],
+                            "comp": []}
+            self.paths = {"null": [],
+                          "comp": []}
+            self.count = 0
+
+            self.pfwid = pdwi
+            self.args.pfwid = pdwi
+            self.doCompact()
